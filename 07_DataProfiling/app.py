0777e6d5951705848f82436c9af0156d68a07132
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-07 10:49:50 +0100

    Add files via upload

--- 07_DataProfiling/app.py
+++ 07_DataProfiling/app.py
@@ -1,4 +1,8 @@
-# pip install pandas-profiling
+# Python 3.10 -> Python 3.11 (not yet)
+
+# pip install pandas-profiling (old version till 1st April 2023)
+# pip install ydata-profiling (new version)
+
 
 import os 
 from pathlib import Path 
@@ -6,8 +10,14 @@ os.chdir(Path(__file__).parent)
 
 
 import pandas as pd 
-from pandas_profiling import ProfileReport 
 
+# Old Version
+#~~~~~~~~~~~~~~
+#from pandas_profiling import ProfileReport # (old version till 1st April 2023)
+
+# New Version
+#~~~~~~~~~~~~~~
+from ydata_profiling import ProfileReport # (new version)
 
 # 1. Read the CSV File
 df = pd.read_csv("./output_my_data.csv")
