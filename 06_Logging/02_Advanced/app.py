61838ead4a25329af98b758f90fc2f478d29db72
rbiotblbk <97528860+rbiotblbk@users.noreply.github.com>
2023-03-07 09:55:56 +0100

    Add files via upload

--- 06_Logging/02_Advanced/app.py
+++ 06_Logging/02_Advanced/app.py
@@ -1,4 +1,5 @@
 import logging 
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
 import os 
 from pathlib import Path 
 os.chdir(Path(__file__).parent)
@@ -15,13 +16,26 @@ formatter = logging.Formatter("%(asctime)s  - %(filename)s - %(name)s - %(leveln
 file_handler = logging.FileHandler("app.log")
 file_handler.setFormatter(formatter)
 
+# Alternative: RotationFileHandler -> File Size -> app.log.1, app.log.2, etc., max Size: 5KByte with 4x Archive Files
+rotation_handler = RotatingFileHandler("app_rotate.log", maxBytes= 5000, backupCount=4)
+rotation_handler.setFormatter(formatter)
+
+# Alternative: TimeRotatingFileHandler -> Via Time
+rotation_time_handler = TimedRotatingFileHandler("app_time.log", when="S", interval= 1, backupCount= 4)
+rotation_time_handler.setFormatter(formatter)
+
 # Stream(Terminal) Handler logger --> log to the terminal(console)
 stream_handler = logging.StreamHandler()
-# stream_handler.setFormatter(formatter)
+stream_handler.setFormatter(formatter)
+
+
+
 
 # Add the handlers to the logger
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
+logger.addHandler(rotation_handler)
+logger.addHandler(rotation_time_handler)
 
 
 
